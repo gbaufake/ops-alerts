e945434696c11eb27e0045d10c38022f5f9d0978
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2017-05-16 19:52:14 -0300

    USE_SSL as parameter on Pushing Triggers

--- ruby/01-push_trigger.rb
+++ ruby/01-push_trigger.rb
@@ -4,13 +4,14 @@ require 'json'
 require 'uri'
 require 'net/http'
 require 'dotenv'
+require 'to_bool'
 
 describe "Pushing Triggers " do
-  it "should be 200 on HTTP Code" do
+  it "should be 200 code of HTTP Response" do
         hawkular_url = ENV['HAWKULAR_URL']
         url = URI(hawkular_url)
         http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = false
+        http.use_ssl = ENV['HAWKULAR_USE_SSL'].to_bool
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         request = Net::HTTP::Post.new(url)
