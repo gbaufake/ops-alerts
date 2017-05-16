3039a41f4b724e8263b84436d4eef61105ed85fa
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2017-05-16 19:25:47 -0300

    Adding DotEnv and Changing SSL to false

--- ruby/01-push_trigger.rb
+++ ruby/01-push_trigger.rb
@@ -1,29 +1,27 @@
 require 'byebug'
 require 'openSSL'
 require 'json'
+require 'uri'
+require 'net/http'
+require 'dotenv'
 
-describe "Pushing Triggers Should be as answer as 200 on HTTP Code" do
-  it "should be true" do
-        require 'uri'
-        require 'net/http'
-
-        url = URI("https://hawkular-metrics.cloud5.jonqe.lab.eng.bos.redhat.com/hawkular/alerts/import/all")
-
+describe "Pushing Triggers " do
+  it "should be 200 on HTTP Code" do
+        hawkular_url = ENV['HAWKULAR_URL']
+        url = URI(hawkular_url)
         http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
+        http.use_ssl = false
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         request = Net::HTTP::Post.new(url)
-        request["hawkular-tenant"] = 'hawkular'
+        request["hawkular-tenant"] = ENV['HAWKULAR_TENANT']
         request["content-type"] = 'application/json'
-        request["authorization"] = 'Basic aGF3a3VsYXI6Rk5BODBqWDlvT3haamdF'
+        request["authorization"] = ENV['HAWKULAR_TOKEN']
         request["cache-control"] = 'no-cache'
-
-        request.body =  JSON.parse(File.read('autoresolve-trigger.json')).to_s
+        request.body =  JSON.parse(File.read('autoresolve-trigger.json')).to_json
 
         response = http.request(request)
-
-        expect(response.code).to   eq(200)
+        expect(response.code).to   eq("200")
 
   end
 end
