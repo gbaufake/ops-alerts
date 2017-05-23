c6d801cba29328311dba64ca5f9f5813c10b691d
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2017-05-23 18:15:03 -0300

    Refactoring on Push Trigger Testing

--- ruby/push_trigger.rb
+++ ruby/push_trigger.rb
@@ -8,26 +8,23 @@ require 'to_bool'
 describe "Pushing Triggers " do
   # Starting the resquest
   before(:all) do
-   if ENV['HAWKULAR_BODY'].to_bool == true
-   body = JSON.parse(File.read('autoresolve-trigger.json')).to_json
-   elsif
-   body = ""
-   end
-    
-    parameters = {:name =>"Pushing Triggers", :hawkular_envirorment => ENV['HAWKULAR_ENVIRONMENT'],
+    @parameters = {:name =>"Pushing Triggers", :hawkular_envirorment => ENV['HAWKULAR_ENVIRONMENT'],
       :hawkular_url => "hawkular/alerts/import/all", :ssl=> ENV['HAWKULAR_USE_SSL'].to_bool, :http_method=> 'POST',
       :hawkular_tenant=> ENV['HAWKULAR_TENANT'], :content_type => 'application/json',
-      :authorization => ENV['HAWKULAR_TOKEN'], :no_cache => 'no-cache' ,
-      :body => body }
-      @test_case = TestCase.new(parameters)
-      @response = @test_case.peform_request
+      :authorization => ENV['HAWKULAR_TOKEN'], :no_cache => 'no-cache' }
+   end
+
+  it "HTTP Response should be 200 if body is not empty" do
+    @parameters[:body] = JSON.parse(File.read('autoresolve-trigger.json')).to_json
+    @test_case = TestCase.new(@parameters)
+    @response = @test_case.peform_request
+    expect(@response.code).to  eq("200")
   end
 
-  it "should be 200 code of HTTP Response if body is not empty and 400 if body is empty" do
-    if ENV['HAWKULAR_BODY'].to_bool == true
-      expect(@response.code).to  eq("200")
-    elsif
-      expect(@response.code).to eq("400")
-     end
-  end
+ it "HTTP Response should be 400 if body is empty" do
+    @parameters[:body] = ''
+    @test_case = TestCase.new(@parameters)
+    @response = @test_case.peform_request
+    expect(@response.code).to  eq("400")
+ end
 end
