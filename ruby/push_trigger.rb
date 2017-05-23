be63892db595a00b4b378dcbb1f94bb8be9eb65d
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2017-05-23 18:00:13 -0300

    Small Modification in order to get 200 if the body is not empty and 400  if the body is empty

--- ruby/push_trigger.rb
+++ ruby/push_trigger.rb
@@ -8,16 +8,26 @@ require 'to_bool'
 describe "Pushing Triggers " do
   # Starting the resquest
   before(:all) do
+   if ENV['HAWKULAR_BODY'].to_bool == true
+   body = JSON.parse(File.read('autoresolve-trigger.json')).to_json
+   elsif
+   body = ""
+   end
+    
     parameters = {:name =>"Pushing Triggers", :hawkular_envirorment => ENV['HAWKULAR_ENVIRONMENT'],
       :hawkular_url => "hawkular/alerts/import/all", :ssl=> ENV['HAWKULAR_USE_SSL'].to_bool, :http_method=> 'POST',
       :hawkular_tenant=> ENV['HAWKULAR_TENANT'], :content_type => 'application/json',
       :authorization => ENV['HAWKULAR_TOKEN'], :no_cache => 'no-cache' ,
-      :body => JSON.parse(File.read('autoresolve-trigger.json')).to_json}
+      :body => body }
       @test_case = TestCase.new(parameters)
       @response = @test_case.peform_request
   end
 
-  it "should be 200 code of HTTP Response" do
-    expect(@response.code).to  eq("200")
+  it "should be 200 code of HTTP Response if body is not empty and 400 if body is empty" do
+    if ENV['HAWKULAR_BODY'].to_bool == true
+      expect(@response.code).to  eq("200")
+    elsif
+      expect(@response.code).to eq("400")
+     end
   end
 end
